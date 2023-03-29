373a79dd793214ec76c32155b085ae18754fff9e
Jeongeun Ha <jeha@dblab.postech.ac.kr>
2023-03-29 19:38:21 +0900

    commit

--- visualize_from_language.py
+++ visualize_from_language.py
@@ -6,6 +6,8 @@ from panel import widgets as w
 from bokeh.models.widgets.tables import NumberFormatter, BooleanFormatter
 import datetime as dt
 from datetime import datetime, date, timedelta, timezone
+import plotly.graph_objs as go
+import plotly.express as px
 
 from functools import partial
 
@@ -58,7 +60,8 @@ css = '''
         border: 1px solid  lightgray;
     }
     '''
-pn.extension('tabulator',sizing_mode = 'stretch_width', css_files=[pn.io.resources.CSS_URLS['font-awesome']], raw_css = [css])
+pn.extension('plotly','tabulator',sizing_mode = 'stretch_width', css_files=[pn.io.resources.CSS_URLS['font-awesome']], raw_css = [css])
+ui = None
 
 def connect(dbtype = 'posgres', host = 'localhost', port = '5432', dbname = 'eda', user = 'username', password = None):
     NotImplemented
@@ -242,6 +245,7 @@ def visualize(conn = None, data = None):
                 self.w_shape = w.Select(name = 'Shape', options = ['None'] + schema[w_measurement.value], width = 300)
             
             self.w_type = w.Select(name = 'Type', options = ['stacked bar', 'bar', 'line', 'area'], width = 300)
+            self.w_order = w.Select(name = 'Order', options = ["ASC", "DESC"], width = 300)
 
             def set_options(event):
                 for widget in self.widget[2][0]:
@@ -256,12 +260,12 @@ def visualize(conn = None, data = None):
                 self.w_data.options = schema[w_measurement.value]
                 if self.w_task.value == 'metrics':
                     
-                    self.widget[1].objects = [pn.Row(self.w_data, self.w_type)]
+                    self.widget[1].objects = [pn.Row(self.w_data, self.w_type, self.w_order)]
                     self.widget[2].objects = [pn.Card(pn.Row(self.w_color, self.w_shape),width =1000, collapsible = True, collapsed = True, title = 'Options')]
 
                 elif self.w_task.value == 'query analysis':
                     
-                    self.widget[1].objects = [pn.Row(self.w_data, self.w_type)]
+                    self.widget[1].objects = [pn.Row(self.w_data, self.w_type, self.w_order)]
                     self.widget[2].objects = [pn.Card(pn.Row(self.w_shape),width =1000, collapsible = True, collapsed = True, title = 'Options')]
                     self.w_color.value = 'queryid'
                 
@@ -311,10 +315,118 @@ def visualize(conn = None, data = None):
     for task in tasks:
         task.w_data.param.watch(set_split, ['value'], onlychanged =True)
 
-    
+    def tasks_to_elements(clicked_button, tasks):
+        for task in tasks:
+            if task.w_task.value == "metrics":
+                df = load_table(conn, w_measurement.value)
+                fig = MetricsElement(y=task.w_data.value,chart_type = 'line').plot(df)
+                print(fig)
+                ui[1].append(pn.pane.Plotly(fig))
+        NotImplemented
     w_draw = w.Button(name='Draw', width = 100)
+    import functools
+    w_draw.on_click(functools.partial(tasks_to_elements, tasks=tasks))
+    ui = pn.Row(get_sidebar(conn), pn.Column("### Visualization Language Generation", widgets, c_task, c_split, w_draw))
+    display(ui)
 
-    display(pn.Row(get_sidebar(conn), pn.Column("### Visualization Language Generation", widgets, c_task, c_split, w_draw)))
+
+    
+
+
+
+class Element:
+    """
+    A class representing a template for visualizing chart.
+
+    Attributes:
+        y (List[str]): A list of column names to use as the y-axis values.
+        chart_type (str): The type of chart to use for visualization.
+        color (Optional[str]): The column name to use for coloring the chart.
+        shape (Optional[str]): The column name to use for shaping the chart.
+        pattern (Optional[str]): The column name to use for patterning the chart.
+        size (Optional[str]): The column name to use for sizing the chart.
+        row (Optional[str]): The column name to use for creating row facets.
+        col (Optional[str]): The column name to use for creating column facets.
+        tab (Optional[str]): The column name to use for creating tab facets.
+        legend (Optional[str]): The column name to use for creating a legend.
+        label (Optional[str]): The column name to use for creating labels.
+    """
+    def __init__(self, x=None, y=None, chart_type= None, color=None, shape=None, pattern=None, size=None, row=None, col=None, tab=None, legend=None, label=None):
+        self.x = x
+        self.y = y
+        self.chart_type = chart_type
+        self.color = color
+        self.shape = shape
+        self.pattern = pattern
+        self.size = size # only numerical
+        self.row = row
+        self.col = col
+        self.tab = tab  
+        #self.legend = legend
+        self.label = label
+        
+    def plot(self, df, title = ""):
+        # implementation of plot method for the base class
+        if self.chart_type == 'scatter':
+            fig = px.scatter(df, x=self.x, y=self.y, color=self.color, symbol = self.shape, size = self.size, facet_col=self.col, facet_row = self.row)
+        elif self.chart_type == 'line':
+            fig = px.line(df, x=self.x, y=self.y, color=self.color, facet_col=self.col, facet_row = self.row)
+        elif self.chart_type == 'bar':
+            fig = px.bar(df, x=self.x, y=self.y, color=self.color, facet_col=self.col)
+        else:
+            raise ValueError("Invalid Chart Type")
+
+        fig.update_layout(
+            title=title,
+            #xaxis_title='Sepal Length (cm)',
+            #yaxis_title='Sepal Width (cm)'
+        )
+        fig.show()
+        return fig
+
+
+class MetricsElement(Element):
+    def __init__(self, y, chart_type, color=None, shape=None, pattern=None, size=None, row=None, col=None, tab=None, legend=None, label=None):
+        super().__init__(chart_type)
+        self.x = 'time'
+        self.y = y
+        self.chart_type = chart_type
+
+class CorrelationElement(Element):
+    def __init__(self, x, y, chart_type, color=None, shape=None, pattern=None, size=None, row=None, col=None, tab=None, legend=None, label=None):
+        super().__init__(chart_type)
+        self.x = x
+        self.y = y
+        self.chart_type = chart_type
+
+
+class QueryAnalysisElement(Element):
+    def __init__(self, y, chart_type, shape=None, pattern=None, size=None, row=None, col=None, tab=None, legend=None, label=None):
+        super().__init__(chart_type)
+        self.x = 'time'
+        self.y = y
+        self.color = 'queryid'
+        self.chart_type = chart_type
+
+
+class DeltaPredictedActualElement(Element):
+    def __init__(self, y, chart_type, color=None, shape=None, pattern=None, size=None, row=None, col=None, tab=None, legend=None, label=None):
+        super().__init__(chart_type, color=color)
+        self.x = 'time'
+        self.y = y
+        self.chart_type = chart_type
+
+
+class QueryRankingElement(Element):
+    def __init__(self, data, max_row=None):
+        super().__init__()
+        self.chart_type = 'table'
+        self.data = data 
+        # Data를 metric과 additional_column으로 분리하는 작업 필요
+        # not used to rank
+        # ㅇself.metrics = metrics # will be used to rank
+        self.max_row = max_row 
+
 
 
     
