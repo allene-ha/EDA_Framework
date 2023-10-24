e50192316e9eadcdc175eb85c6403d0466c3bcee
Jeongeun Ha <jeha@dblab.postech.ac.kr>
2023-10-24 05:06:45 +0000

    commit

---
+++ benchmark/prepare.sh
@@ -0,0 +1,15 @@
+# 데이터베이스 이름
+db_name="oltpbenchtest"
+
+# 데이터베이스가 이미 존재하는지 확인
+if psql -lqt | cut -d \| -f 1 | grep -qw $db_name; then
+    echo "Database $db_name already exists."
+else
+    # 데이터베이스 생성
+    createdb $db_name
+    echo "Database $db_name created."
+fi
+
+echo "Prepare OLTP Benchmark"
+sysbench --db-driver=pgsql --pgsql-user=postgres --pgsql-password=postgres --pgsql-db=oltpbench --table_size=800000 --tables=150 --threads=64 --time=1800 --report-interval=60 oltp_read_write prepare
+
---
+++ benchmark/run.sh
@@ -0,0 +1,22 @@
+# 실행 예시: ./run.sh 64 214
+
+
+#!/bin/bash
+
+# $1은 normal thread 개수, $2는 anomaly thread 개수로 가정
+normal_thread_count="$1"
+anomaly_thread_count="$2"
+
+# normal 명령어 생성
+normal="sysbench --db-driver=pgsql --pgsql-user=postgres --pgsql-password=postgres --pgsql-db=oltpbench --table_size=800000 --tables=150 --threads=$normal_thread_count --time=1800 --report-interval=60 oltp_read_write run"
+
+# anomaly 명령어 생성
+anomaly="sysbench --db-driver=pgsql --pgsql-user=postgres --pgsql-password=postgres --pgsql-db=oltpbench --table_size=800000 --tables=150 --threads=$anomaly_thread_count --time=300 --report-interval=60 oltp_read_write run"
+
+# "normal" 명령어 실행
+echo "Running normal: $normal"
+$normal
+
+# "anomaly" 명령어 실행
+echo "Running anomaly: $anomaly"
+$anomaly
